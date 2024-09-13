33834264a9de15768acbd246e00e46ed1b454c0c
ZS-nus <e1352976@u.nus.edu>
2024-09-13 15:57:20 +0800

    Added the collected data from ESG reports

---
+++ groupB/scripts/db_connect.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import pymysql
+import mysql.connector
+
+endpoint = 'my-rds-instance.123456789012.us-east-1.rds.amazonaws.com'
+username = 'admin'
+password = 'mypassword'
+database = 'mydatabase'
+# Set the database credentials
+host = '<YOUR_RDS_ENDPOINT>'
+port = 3306
+user = '<YOUR_DATABASE_USERNAME>'
+password = '<YOUR_DATABASE_PASSWORD>'
+database = '<YOUR_DATABASE_NAME>'
+
+# Connect to the database
+connection = pymysql.connect(
+    host=host,
+    port=port,
+    user=user,
+    password=password,
+    database=database
+)
+
+# Create a cursor object
+cursor = connection.cursor()
+
+# Execute a SQL query
+cursor.execute('SELECT * FROM environment')
+
+# Fetch the results
+results = cursor.fetchall()
+
+# Print the results
+for result in results:
+    print(result)
+
+# Close the cursor and connection
+cursor.close()
+connection.close()
+
